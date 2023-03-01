7485e31a4a16d5ab698303d6bc49e04cd2c429af
gabrielapinheiroc <mgpc1905@gmail.com>
2023-03-01 09:36:43 -0300

    add more tables

--- createtable.sql
+++ createtable.sql
@@ -25,6 +25,7 @@ CREATE TABLE Telefone_funcionario (
 
 
 
+
 CREATE TABLE Revendedora (
     cnpj VARCHAR2(11) NOT NULL, 
 
@@ -34,7 +35,7 @@ CREATE TABLE Revendedora (
 -- IN PROGRESS
 
 CREATE TABLE Peca (
-    id VARCHAR2(10),
+    id VARCHAR2(10) NOT NULL,
     categoria VARCHAR2(1),  -- template -- A, B or C
     maquina_inspetora VARCHAR2(10),  -- cod
     veiculo_recebedor VARCHAR2(17),  -- n_chassi
@@ -47,6 +48,15 @@ CREATE TABLE Peca (
 
 )
 
+CREATE TABLE Relatorio_manutencao(
+    num_relatorio VARCHAR2(10) NOT NULL, 
+    data date NOT NULL, 
+    valor number(7,2),
+
+    CONSTRAINT Relatorio_manutencao_pkey PRIMARY KEY (num_relatorio)
+
+);
+
 CREATE TABLE Telefone_revendedora (
     cod_revendedora VARCHAR2(11) NOT NULL,
     num_telefone VARCHAR2(13) NOT NULL, -- template -- (81) 8888-8888
@@ -56,6 +66,32 @@ CREATE TABLE Telefone_revendedora (
     CONSTRAINT telefone_revendedora_fkey FOREIGN KEY (cod_revendedora) REFERENCES Revendedora(cnpj)
 
 );
+
+ CREATE TABLE Vender (
+    cnpj_revendedora VARCHAR2(11),
+    cpf_funcionario VARCHAR2(11),
+    n_chassi VARCHAR2(17),
+    
+    CONSTRAINT Vender_pkey PRIMARY KEY (cnpj_revendedor*, cpf_funcionario, n_chassi),
+
+    CONSTRAINT Vender_fkey1 FOREIGN KEY (cnpj_revendedora) REFERENCES Revendedora(cnpj),
+    CONSTRAINT Vender_fkey2 FOREIGN KEY (cpf_funcionario) REFERENCES Funcionario(cpf),
+    CONSTRAINT Vender_fkey3 FOREIGN KEY (n_chassi) REFERENCES Datas_venda(n_chassi)
+
+ );
+
+CREATE TABLE Produzir(
+    cod_maquina_montagem VARCHAR2(10) NOT NULL, 
+    n_chassin_chassi VARCHAR2(17) NOT NULL, 
+    cpf_funcionario VARCHAR2(11),
+    
+    CONSTRAINT Produzir_pkey PRIMARY KEY (cod_maquina_montagem, n_chassi),
+
+    CONSTRAINT Produzir_fkey1 FOREIGN KEY (cod_maquina_montagem) REFERENCES Maquina_montagem(codigo_identificacao),
+    CONSTRAINT Produzir_fkey2 FOREIGN KEY (n_chassi) REFERENCES Veiculo(n_chassi),
+    CONSTRAINT Produzir_fkey3 FOREIGN KEY (cpf_funcionario) REFERENCES Funcionario(cpf)
+);
+
 
 -- #DUVIDA: o que fazer quando a chave primaria é uma chave estrangeira?
 CREATE TABLE Data_venda (
