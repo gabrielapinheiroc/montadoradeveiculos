3a8999eec20a375a31b8c71fefcbddf8763580c8
pedroalfonseca <paalf1337@gmail.com>
2023-03-02 00:15:42 -0300

    Povoamento Maquina adicionado

--- povoamento.sql
+++ povoamento.sql
@@ -118,6 +118,58 @@ VALUES
         '00000000006'
     );
 
+
+-- Maquina
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000001', 'Modelo A', '20-FEB-2018');
+
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000002', 'Modelo B', '01-APR-2018');
+
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000003', 'Modelo C', '16-JUL-2018');
+
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000004', 'Modelo D', '12-OCT-2018');
+
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000005', 'Modelo E', '30-JAN-2018');
+
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000006', 'Modelo F', '08-AUG-2019');
+
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000007', 'Modelo G', '23-DEC-2019');
+
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000008', 'Modelo H', '12-MAY-2020');
+
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000009', 'Modelo I', '27-SEP-2020');
+
+INSERT INTO
+    Maquina (codigo_identificacao, modelo, data_fabricacao)
+VALUES
+    ('M000000010', 'Modelo J', '03-NOV-2020');
+
 -- Telefone_funcionario
 INSERT INTO
     Telefone_funcionario (cpf_funcionario, num_telefone)
