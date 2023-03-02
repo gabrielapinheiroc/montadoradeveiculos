1f283704826a0aa5e45944ccf9035b11cd9d239f
gabrielapinheiroc <mgpc1905@gmail.com>
2023-03-01 22:47:27 -0300

    creating new tables

--- createtable.sql
+++ createtable.sql
@@ -87,11 +87,44 @@ CREATE TABLE Custo_veiculo(
 
     CONSTRAINT custo_veiculo_pkey PRIMARY KEY (modelo, cor, ano),
 
-    CONSTRAINT custo_veiculo_fkey1 FOREIGN KEY (modelo) REFERENCES Veiculo(modelo),
-    CONSTRAINT custo_veiculo_fkey2 FOREIGN KEY (cor) REFERENCES Veiculo(cor),
-    CONSTRAINT custo_veiculo_fkey3 FOREIGN KEY (ano) REFERENCES Veiculo(ano)
+    CONSTRAINT custo_veiculo_fkey FOREIGN KEY (modelo, cor, ano) REFERENCES Veiculo(modelo, cor, ano)
+    --CONSTRAINT custo_veiculo_fkey1 FOREIGN KEY (modelo) REFERENCES Veiculo(modelo),
+    --CONSTRAINT custo_veiculo_fkey2 FOREIGN KEY (cor) REFERENCES Veiculo(cor),
+    --CONSTRAINT custo_veiculo_fkey3 FOREIGN KEY (ano) REFERENCES Veiculo(ano)
 );
 
+CREATE TABLE Revendedora (
+    cnpj VARCHAR2(14) NOT NULL, 
+
+    CONSTRAINT revendedora_pkey PRIMARY KEY (cnpj)
+);
+
+-- OK
+CREATE TABLE Peca (
+    id VARCHAR2(10) NOT NULL,
+    categoria VARCHAR2(1),  -- template -- A, B or C
+    maquina_inspetora VARCHAR2(10),  -- cod
+    veiculo_recebedor VARCHAR2(17),  -- n_chassi
+    data_inspecao date,
+
+    CONSTRAINT peca_pkey PRIMARY KEY (id),
+
+    CONSTRAINT peca_fkey1 FOREIGN KEY (maquina_inspetora) REFERENCES Maquina_controle_qualidade(codigo_identificacao),
+    CONSTRAINT Peca_fkey2 FOREIGN KEY (veiculo_recebedor) REFERENCES Veiculo(n_chassi)
+);
+
+-- NEW
+CREATE TABLE Historico_manutencao (
+    codigo_identificacao_maquina VARCHAR2(10) NOT NULL, 
+    data_ date, 
+    valor number(9,2),
+
+    CONSTRAINT historico_manutencao_pkey PRIMARY KEY (codigo_identificacao_maquina,data_),
+
+    CONSTRAINT historico_manutencao_fkey FOREIGN KEY (codigo_identificacao_maquina) REFERENCES Maquina(codigo_identificacao)
+);
+
+-- OK
 CREATE TABLE Telefone_funcionario (
     cpf_funcionario VARCHAR2(11) NOT NULL,
     num_telefone VARCHAR2(13) NOT NULL, -- template -- (81) 8888-8888
@@ -101,38 +134,6 @@ CREATE TABLE Telefone_funcionario (
     CONSTRAINT telefone_funcionario_fkey FOREIGN KEY (cpf_funcionario) REFERENCES Funcionario(cpf)
 );
 
-
-
-
-CREATE TABLE Revendedora (
-    cnpj VARCHAR2(14) NOT NULL, 
-
-    CONSTRAINT revendedora_pkey PRIMARY KEY (cnpj)
-);
-
--- IN PROGRESS
-
-CREATE TABLE Peca (
-    id VARCHAR2(10) NOT NULL,
-    categoria VARCHAR2(1),  -- template -- A, B or C
-    maquina_inspetora VARCHAR2(10),  -- cod
-    veiculo_recebedor VARCHAR2(17),  -- n_chassi
-    data_inspecao date,
-
-    CONSTRAINT Peca_pkey PRIMARY KEY (id),
-
-    CONSTRAINT Peca_fkey1 FOREIGN KEY (maquina_inspetora) REFERENCES Maquina_controle_qualidade(codigo_identificacao),
-    CONSTRAINT Peca_fkey2 FOREIGN KEY (veiculo_recebedor) REFERENCES Veiculo(n_chassi)
-);
-
-CREATE TABLE Relatorio_manutencao(
-    num_relatorio VARCHAR2(10) NOT NULL, 
-    data date NOT NULL, 
-    valor number(7,2),
-
-    CONSTRAINT Relatorio_manutencao_pkey PRIMARY KEY (num_relatorio)
-
-);
  -- OK
 CREATE TABLE Telefone_revendedora (
     cod_revendedora VARCHAR2(14) NOT NULL, -- cnpjrevendedora
@@ -144,40 +145,27 @@ CREATE TABLE Telefone_revendedora (
 
 );
 
- CREATE TABLE Vender (
+CREATE TABLE Vender (
     cnpj_revendedora VARCHAR2(14),
     cpf_funcionario VARCHAR2(11),
     n_chassi VARCHAR2(17),
     
-    CONSTRAINT Vender_pkey PRIMARY KEY (cnpj_revendedor*, cpf_funcionario, n_chassi),
+    CONSTRAINT vender_pkey PRIMARY KEY (cnpj_revendedor, cpf_funcionario, n_chassi),
 
-    CONSTRAINT Vender_fkey1 FOREIGN KEY (cnpj_revendedora) REFERENCES Revendedora(cnpj),
-    CONSTRAINT Vender_fkey2 FOREIGN KEY (cpf_funcionario) REFERENCES Funcionario(cpf),
-    CONSTRAINT Vender_fkey3 FOREIGN KEY (n_chassi) REFERENCES Datas_venda(n_chassi)
-
- );
+    CONSTRAINT vender_fkey1 FOREIGN KEY (cnpj_revendedora) REFERENCES Revendedora(cnpj),
+    CONSTRAINT vender_fkey2 FOREIGN KEY (cpf_funcionario) REFERENCES Funcionario(cpf),
+    CONSTRAINT vender_fkey3 FOREIGN KEY (n_chassi) REFERENCES Veiculo(n_chassi)
+);
 
 CREATE TABLE Produzir(
     cod_maquina_montagem VARCHAR2(10) NOT NULL, 
     n_chassin_chassi VARCHAR2(17) NOT NULL, 
     cpf_funcionario VARCHAR2(11),
     
-    CONSTRAINT Produzir_pkey PRIMARY KEY (cod_maquina_montagem, n_chassi),
+    CONSTRAINT produzir_pkey PRIMARY KEY (cod_maquina_montagem, n_chassi),
 
-    CONSTRAINT Produzir_fkey1 FOREIGN KEY (cod_maquina_montagem) REFERENCES Maquina_montagem(codigo_identificacao),
-    CONSTRAINT Produzir_fkey2 FOREIGN KEY (n_chassi) REFERENCES Veiculo(n_chassi),
-    CONSTRAINT Produzir_fkey3 FOREIGN KEY (cpf_funcionario) REFERENCES Funcionario(cpf)
+    CONSTRAINT produzir_fkey1 FOREIGN KEY (cod_maquina_montagem) REFERENCES Maquina_montagem(codigo_identificacao),
+    CONSTRAINT produzir_fkey2 FOREIGN KEY (n_chassi) REFERENCES Veiculo(n_chassi),
+    CONSTRAINT produzir_fkey3 FOREIGN KEY (cpf_funcionario) REFERENCES Funcionario(cpf)
 );
 
-
--- #DUVIDA: o que fazer quando a chave primaria é uma chave estrangeira?
-CREATE TABLE Data_venda (
-    n_chassi VARCHAR2(17) NOT NULL,
-    data_chegada_revendedora date NOT NULL, 
-    data_saida_montadora date NOT NULL,
-
-    CONSTRAINT Data_venda_pkey PRIMARY KEY (n_chassi),
-
-    CONSTRAINT Data_venda_fkey FOREIGN KEY (n_chassi) REFERENCES Veiculo(n_chassi)
-
-);
