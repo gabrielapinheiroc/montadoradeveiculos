c048310150090b7af4069cd30c31fc9f90e9c734
Gabriel Queiroz <gqmv@cin.ufpe.br>
2023-03-08 14:55:18 -0300

    Inserted sql consults up to n15

--- consultas.sql
+++ consultas.sql
@@ -41,3 +41,64 @@ FROM
     Custo_veiculo
 WHERE
     custo_producao > 30000;
+
+SELECT
+    modelo,
+    custo_producao
+FROM
+    Custo_veiculo
+WHERE
+    custo_producao BETWEEN 15000 AND 30000;
+
+SELECT
+    cpf_funcionario,
+    n_chassi
+FROM
+    Vender
+WHERE
+    cpf_funcionario IN ('00000000001', '00000000002', '00000000004');
+
+SELECT
+    primeiro_nome,
+    cpf,
+    funcao
+FROM
+    Funcionario
+WHERE
+    funcao LIKE 'Head de %';
+
+SELECT
+    primeiro_nome,
+    cpf,
+    funcao
+FROM
+    Funcionario
+WHERE
+    email IS NOT NULL;
+
+SELECT
+    f.primeiro_nome,
+    t.num_telefone
+FROM
+    Funcionario f
+    INNER JOIN Telefone_funcionario t ON f.cpf = t.cpf_funcionario;
+
+SELECT
+    MAX(capacidade)
+FROM
+    Linha_montagem;
+
+SELECT
+    MIN(ano)
+FROM
+    Veiculo;
+
+SELECT
+    AVG(custo_producao)
+FROM
+    Custo_veiculo;
+
+SELECT
+    COUNT(*)
+FROM
+    Peca;
