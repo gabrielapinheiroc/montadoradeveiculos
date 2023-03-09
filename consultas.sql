f22ab91723f48893681180e1e0a37a64f0f58a53
Gabriel Queiroz <gqmv@cin.ufpe.br>
2023-03-08 22:46:43 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -154,3 +154,45 @@ WHERE
         WHERE
             cnpj_revendedora = '00000000000003'
     );
+
+SELECT
+    F.primeiro_nome,
+    F.cpf_funcionario
+FROM
+    Funcionario F
+WHERE
+    F.salario > (
+        SELECT
+            AVG(Salario)
+        FROM
+            Funcionario
+    );
+
+SELECT
+    cpf_funcionario,
+    n_chassi
+FROM
+    Vender
+WHERE
+    cpf_funcionario IN (
+        SELECT
+            F.cpf
+        FROM
+            Funcionario F
+        WHERE
+            F.supervisor = '00000000001'
+    );
+
+SELECT
+    LM.capacidade
+FROM
+    Linha_montagem LM
+WHERE
+    LM.num_linha = ALL (
+        SELECT
+            MM.linha_montagem
+        FROM
+            Maquina_montagem MM
+        WHERE
+            MM.codigo_identificacao = '0000000001'
+    );
