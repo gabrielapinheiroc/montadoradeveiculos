af462d5a4c6cdcd62219da0588e9435766b5dc92
gabrielapinheiroc <mgpc1905@gmail.com>
2023-03-08 22:10:34 -0300

    consulta 17 e 18 correct

--- consulta17to26.sql
+++ consulta17to26.sql
@@ -1,11 +1,6 @@
 SELECT F.primeiro_nome, F.cpf_funcionario FROM Funcionario F
 WHERE F.salario > (SELECT AVG(Salario) FROM Funcionario);
 
-SELECT 
-FROM
-WHERE IN (
-    SELECT 
-)
 
 SELECT
     cpf_funcionario,
