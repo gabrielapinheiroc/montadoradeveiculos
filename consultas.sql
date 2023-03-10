a5442968ae06ddacdaabe57c5623a4388b476e77
gabrielapinheiroc <mgpc1905@gmail.com>
2023-03-09 22:47:50 -0300

    add consultaspl.sql to consultas

--- consultas.sql
+++ consultas.sql
@@ -287,3 +287,36 @@ loop
 end loop;
 DBMS_OUTPUT.PUT_LINE('Precisamos demitir todos os funcionarios que ganham mais que ' || gasto_total);
     end;
+
+DECLARE    
+    nv_num_linha INTEGER := 2;
+    nv_capacidade INTEGER;
+    nv_nivelcapacidade VARCHAR2(255);
+BEGIN
+    SELECT capacidade INTO nv_capacidade FROM Linha_montagem WHERE num_linha = nv_num_linha;
+    CASE nv_capacidade
+        WHEN 10 THEN nv_nivelcapacidade := 'nivel A';
+        WHEN 11 THEN nv_nivelcapacidade := 'nivel B';
+        WHEN 12 THEN nv_nivelcapacidade := 'nivel C';
+        WHEN 13 THEN nv_nivelcapacidade := 'nivel D';
+        WHEN 14 THEN nv_nivelcapacidade := 'nivel E';
+        WHEN 15 THEN nv_nivelcapacidade := 'nivel F';
+        WHEN 16 THEN nv_nivelcapacidade := 'nivel G';
+        WHEN 17 THEN nv_nivelcapacidade := 'nivel H';
+        WHEN 18 THEN nv_nivelcapacidade := 'nivel I';
+    END CASE;
+        dbms_output.put_line(nv_nivelcapacidade);
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+        dbms_output.put_line('Não foi possivel identificar a capacidade');
+END;
+
+DECLARE
+    nv_telefone_revendedora Telefone_revendedora%ROWTYPE;
+BEGIN
+    nv_telefone_revendedora.cod_revendedora := '00000000000111';
+    nv_telefone_revendedora.num_telefone := '(99)9456-9989';
+    
+    dbms_output.put_line(nv_telefone_revendedora.cod_revendedora);
+    dbms_output.put_line(nv_telefone_revendedora.num_telefone);
+END;
