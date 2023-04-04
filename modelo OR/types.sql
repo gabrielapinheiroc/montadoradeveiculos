89f360f36484b7f5d72d6750a30c0a91d758e9ca
Gabriela Pinheiro <gabrielapinheiroc>
2023-04-04 12:51:05 -0300

    type funcionario

--- modelo OR/types.sql
+++ modelo OR/types.sql
@@ -1,13 +1,25 @@
 --FUNCINÁRIO--
 CREATE OR REPLACE TYPE tp_telefone_funcionario AS OBJECT(
-    telefone varchar2(13)
+    telefone VARCHAR2(13)
 );
 /
 
 CREATE OR REPLACE TYPE tp_telefone_funcionario_array AS VARRAY(2) OF tp_telefone_funcionario;
 -- funcionario tem 2 telefones (pessoal e residencial)
 /
+
+CREATE OR REPLACE TYPE tp_funcionario AS OBJECT(
+    cpf VARCHAR2(11),
+    primeiro_nome varchar2(50),
+    sobrenome varchar2(50),
+    salario number(9, 2),
+    funcao varchar2(30),
+    supervisor REF tp_funcionario -- autorelacionamento, criar uma var do mesmo tipo.
+
+)NOT FINAL NOT INSTANTIABLE ;
+/
 --REVENDEDORA--
+
 
 --MAQUINA--
 
