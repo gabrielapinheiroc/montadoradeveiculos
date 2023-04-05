0a4399a260985c35c242ec7060cf4d54b687d919
Maria Claudino <maria.claudino@tpfe.com.br>
2023-04-04 23:32:24 -0300

    tables maquina

--- modelo OR/table_.sql
+++ modelo OR/table_.sql
@@ -5,6 +5,30 @@ CREATE TABLE tb_funcionario OF tp_funcionario (
     salario       NOT NULL,
     funcao        NOT NULL,
     telefone      NOT NULL,
-    supervisor SCOPE IS tb_tripulante
+    supervisor SCOPE IS tb_funcionario
 );
 /
+
+CREATE TABLE tb_maquina_montagem OF tp_maquina_montagem (
+    codigo_identificacao PRIMARY KEY,
+    modelo               NOT NULL,
+    nome_modelo          NOT NULL,
+    fabricante           NOT NULL,
+    data_fabricacao      NOT NULL,
+    linha_montagem NOT NULL,
+    num_linha      NOT NULL,
+    capacidade     NOT NULL,
+);
+/
+
+CREATE TABLE tb_controle_qualidade OF tp_controle_qualidade (
+    codigo_identificacao PRIMARY KEY,
+    modelo               NOT NULL,
+    nome_modelo          NOT NULL,
+    fabricante           NOT NULL,
+    data_fabricacao      NOT NULL,
+    parametros_teste   NOT NULL,
+    limites_tolerancia NOT NULL,
+);
+/
+
