866b3c4fedc5714c018158ca9b9ac827068e0a94
Maria Claudino <maria.claudino@tpfe.com.br>
2023-04-05 01:04:28 -0300

    INITAL POVOAMENTO

---
+++ modelo OR/povoamento_.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_funcionario VALUES(
+    tp_funcionario(
+        '00000000001',
+        'João', 
+        'Silva', 
+        90000, 
+        'CEO'
+        '(99)9999-9999',
+        NULL
+    )
+);
--- modelo OR/table_.sql
+++ modelo OR/table_.sql
@@ -1,3 +1,4 @@
+--ok
 CREATE TABLE tb_funcionario OF tp_funcionario (
     CPF PRIMARY KEY,
     primeiro_nome NOT NULL,
@@ -8,27 +9,52 @@ CREATE TABLE tb_funcionario OF tp_funcionario (
     supervisor SCOPE IS tb_funcionario
 );
 /
-
+-- ok
 CREATE TABLE tb_maquina_montagem OF tp_maquina_montagem (
     codigo_identificacao PRIMARY KEY,
-    modelo               NOT NULL,
     nome_modelo          NOT NULL,
     fabricante           NOT NULL,
     data_fabricacao      NOT NULL,
-    linha_montagem NOT NULL,
+    --linha_montagem NOT NULL,
     num_linha      NOT NULL,
-    capacidade     NOT NULL,
+    capacidade     NOT NULL
+
 );
 /
-
-CREATE TABLE tb_controle_qualidade OF tp_controle_qualidade (
+-- ok
+CREATE TABLE tb_maquina_controle_qualidade OF tp_maquina_controle_qualidade (
     codigo_identificacao PRIMARY KEY,
-    modelo               NOT NULL,
     nome_modelo          NOT NULL,
     fabricante           NOT NULL,
     data_fabricacao      NOT NULL,
     parametros_teste   NOT NULL,
-    limites_tolerancia NOT NULL,
+    limites_tolerancia NOT NULL
 );
 /
 
+CREATE TABLE tb_revendedora OF tp_revendedora (
+    cnpj PRIMARY KEY,
+    telefone NOT NULL
+);
+/
+
+CREATE TABLE tb_producao OF tp_producao;
+/
+
+CREATE TABLE tb_veiculo OF tp_veiculo (
+    n_chassi PRIMARY KEY,
+    modelo NOT NULL,
+    cor NOT NULL,
+    ano NOT NULL,
+    custo_producao NOT NULL
+);
+/
+
+CREATE TABLE tb_peca OF tp_peca (
+    id PRIMARY KEY,
+    categoria NOT NULL,
+    maquina_inspetora WITH ROWID REFERENCES tb_maquina_controle_qualidade,
+    veiculo_recebedor WITH ROWID REFERENCES tb_veiculo,
+    data_inspecao_ NOT NULL
+);
+/
